421a329b35864f98f4d702fadf734ec44a7b1381
Marcelo Viegas <mbrunoviegas@gmail.com>
2025-02-11 21:15:07 -0300

    feat: reports

--- src/CashFlow.Application/UseCases/Expenses/Report/ApplicationExtensionsKeys.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/ApplicationExtensionsKeys.cs
@@ -2,4 +2,5 @@
 public static class ApplicationExtensionsKeys
 {
     public const string EXCEL_REPORT_EXPENSE_USE_CASE = "ExcelReportExpenseUseCase";
+    public const string PDF_REPORT_EXPENSE_USE_CASE = "PdfReportExpenseUseCase";
 }
